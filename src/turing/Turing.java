799fa48b679d0b69c532983ce68f0900305fc233
Capiroto <51887681+diegoluanmartins@users.noreply.github.com>
2023-07-21 22:56:57 -0300

    DistanceArray

--- src/turing/Turing.java
+++ src/turing/Turing.java
@@ -2,10 +2,12 @@ package turing;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Turing {
     
-    public static List<String> Action(String morsecode){
+    public static List<String> getMorseCode(String morsecode){
         List<String> results = new ArrayList<>();
         String previous = morsecode.substring(0, 1);
         for (int i = 1; i < morsecode.length(); i++) {
@@ -21,8 +23,29 @@ public class Turing {
         return results;
     }
 
+    public static List<Integer> getDistanceArray(String code, char c){
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> distance = new ArrayList<>();
+        for(int i = 0; i < code.length(); i++){
+            if(c == code.charAt(i)){
+                queue.add(i);
+            }
+        }
+        int closestId = queue.remove();
+        System.out.println("closestId = " + closestId);
+        for(int i = 0; i < code.length(); i++){
+            if(!queue.isEmpty() && (Math.abs(closestId - i) > Math.abs(queue.peek() - i))){
+                closestId = queue.remove();
+            }
+            distance.add(Math.abs(closestId - i));
+        }
+        return distance;
+    }
+
+    
+
     public static void main(String[] args) throws Exception {
-        System.out.println(Action("..--...."));
-        System.out.println(Action("...."));
+        System.out.println(getMorseCode("..--...."));
+        System.out.println(getMorseCode("...."));
     }
 }
